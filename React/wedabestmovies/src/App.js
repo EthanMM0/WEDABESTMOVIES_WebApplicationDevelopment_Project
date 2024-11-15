95e3ca5cae0579f1d61181b00dd3678aa50bd050
EthanMM0 <ethan.m.micciola@gmail.com>
2024-11-15 11:27:04 -0500

    modified:   React/wedabestmovies/src/App.js 	modified:   React/wedabestmovies/src/components/CartState.jsx 	modified:   React/wedabestmovies/src/components/Landing.css 	new file:   React/wedabestmovies/src/images/Batman.png 	new file:   React/wedabestmovies/src/images/Dune.png 	new file:   React/wedabestmovies/src/images/Smile.png 	new file:   React/wedabestmovies/src/images/Spider-man-no-way-home.png 	modified:   React/wedabestmovies/src/images/WDBM.png 	modified:   React/wedabestmovies/src/pages/Landing.jsx

--- React/wedabestmovies/src/App.js
+++ React/wedabestmovies/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing'; 
+import Checkout from './pages/Checkout'; 
+import { CartProvider } from './components/CartState';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <CartProvider> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
+  </CartProvider>
+);
 
 export default App;
