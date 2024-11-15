57a94347cd30bd6774f22604dec71030895dbb1e
EthanMM0 <ethan.m.micciola@gmail.com>
2024-11-15 11:41:03 -0500

    Updated App.js

--- React/wedabestmovies/src/App.js
+++ React/wedabestmovies/src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import Landing from './pages/Landing'; 
-import Checkout from './pages/Checkout'; 
-=======
 import Landing from './pages/Landing';
->>>>>>> 5ba026714407cf1ff662944b671eb785cf9d1665
 import { CartProvider } from './components/CartState';
 
 const App = () => (
@@ -13,10 +8,6 @@ const App = () => (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-<<<<<<< HEAD
-        <Route path="/checkout" element={<Checkout />} />
-=======
->>>>>>> 5ba026714407cf1ff662944b671eb785cf9d1665
       </Routes>
     </Router>
   </CartProvider>
