d1575487efd9deff1d22b0ab5481c3c2c8390835
EthanMM0 <ethan.m.micciola@gmail.com>
2024-11-15 10:56:19 -0500

    Update App.js

--- React/wedabestmovies/src/App.js
+++ React/wedabestmovies/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing'; 
+import Landing from './pages/Landing';
+import { CartProvider } from './components/CartState';
 
 const App = () => (
   <CartProvider> 
